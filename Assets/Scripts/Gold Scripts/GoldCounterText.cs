ebf3f4603414777ec472be6df4ef2978acf2a41a
Eray Celik <celikeray0644@gmail.com>
2025-07-10 19:53:29 +0300

    Game Over Panel  Redesigned.

--- Assets/Scripts/Gold Scripts/GoldCounterText.cs
+++ Assets/Scripts/Gold Scripts/GoldCounterText.cs
@@ -4,11 +4,8 @@ public class GoldCounterText : MonoBehaviour
 {
     public static int goldCounter = 0;
     [SerializeField] private TextMeshProUGUI goldText;
-    [SerializeField] private TextMeshProUGUI goldTextEndGame;
-
     void Update()
     {
         goldText.text = goldCounter.ToString(); // Integer'ı stringe çevirdik
-        goldTextEndGame.text = goldCounter.ToString();
     }
 }
